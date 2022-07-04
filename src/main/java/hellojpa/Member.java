ea7a4246ec8cfb33d404e1710f3a640a16cd3912
yongjo <yj92133581@gmail.com>
2022-07-04 23:09:30 +0900

    다양한 연관관계 매핑-다대다 [N:M]

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,11 +3,14 @@ package hellojpa;
 import org.hibernate.annotations.JoinColumnOrFormula;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Member {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
 
@@ -21,6 +24,9 @@ public class Member {
     @OneToOne
     @JoinColumn(name = "LOCKER_ID")
     private Locker locker;
+
+    @OneToMany(mappedBy = "member")
+    private List<MemberProduct> memberProducts = new ArrayList<>();
 
     public Long getId() {
         return id;
---
+++ src/main/java/hellojpa/MemberProduct.java
@@ -0,0 +1,28 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class MemberProduct {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    private int count;
+    private int price;
+
+    private LocalDateTime orderDateTime;
+
+
+}
