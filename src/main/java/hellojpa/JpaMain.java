ef3f5fd7e3e12b48f11de5386ccdd849dede3be8
yongjo <yjlee90@suresofttech.com>
2022-07-05 14:07:55 +0900

    프록시와 연관관계 관리-즉시 로딩과 지연 로딩

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,6 +3,7 @@ package hellojpa;
 import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -14,17 +15,35 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Team teamB = new Team();
+            teamB.setName("teamB");
+            em.persist(teamB);
 
             Member member1 = new Member();
             member1.setUsername("member1");
+            member1.setTeam(team);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setTeam(teamB);
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            Member refMember = em.getReference(Member.class, member1.getId());
-            System.out.println("refMember = " + refMember.getClass()); //Proxy
-            Hibernate.initialize(refMember); //강제초기화
+//            Member m = em.find(Member.class, member1.getId());
+
+            List<Member> members = em.createQuery("select m from Member m join fetch m.team", Member.class)
+                    .getResultList();
+
+            //SQL: select * from Member
+            //SQL: select * from Team where TEAM_ID = xxx
+            
 
             tx.commit();
         } catch (Exception e) {
